70e467da334a7ae7a01a839bcb9640fa87ae2121
msm-faraj <farajzademeysam@gmail.com>
2024-01-21 23:40:21 +0100

    expence form just added

---
+++ app-client/src/components/expence/ExpenceForm.tsx
@@ -0,0 +1,77 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
+  Stack,
+} from "@chakra-ui/react";
+
+interface Props {
+  accounts: string[];
+  categories: string[];
+}
+
+export const ExpenceForm = ({ accounts, categories }: Props) => {
+  return (
+    <form>
+      <Stack>
+        {/* Account */}
+        <FormControl>
+          <FormLabel>Account</FormLabel>
+          <Select placeholder="Select country">
+            {accounts.map((account) => (
+              <option>{account}</option>
+            ))}
+          </Select>
+        </FormControl>
+
+        {/* Date */}
+        <FormControl>
+          <FormLabel>Date & Time</FormLabel>
+          <Input type="datetime-local" placeholder="Date & Time" />
+        </FormControl>
+
+        {/* Category */}
+        <FormControl>
+          <FormLabel>Category</FormLabel>
+          <Select placeholder="Select country">
+            {categories.map((category) => (
+              <option>{category}</option>
+            ))}
+          </Select>
+        </FormControl>
+
+        {/* Amount */}
+        <FormControl>
+          <FormLabel>Amount</FormLabel>
+          <NumberInput max={1000000000000000} min={0}>
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </FormControl>
+
+        {/* Note */}
+        <FormControl>
+          <FormLabel>Note</FormLabel>
+          <Input type="text" placeholder="Note" />
+        </FormControl>
+
+        {/* Description */}
+        <FormControl>
+          <FormLabel>Descripton</FormLabel>
+          <Input type="text" placeholder="Descripton" />
+        </FormControl>
+        <Button>Create Expence</Button>
+      </Stack>
+    </form>
+  );
+};
