7b77a09ffed9d15338ba03c2f2af77bfcc979fe7
msm-faraj <farajzademeysam@gmail.com>
2024-02-11 19:51:21 +0100

    user, habit, task, event tables created

---
+++ app-server/migrations/20240211183741-create-task.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      startedAt: {
+        type: Sequelize.DATE
+      },
+      endAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tasks');
+  }
+};
---
+++ app-server/models/event.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  event.init({
+    name: DataTypes.STRING,
+    time: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'event',
+  });
+  return event;
+};
